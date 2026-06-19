7a4836e4986daced5a656455dd71100dcd7bf4fa
Radoslaw Grzanka <radoslawg@gmail.com>
2026-06-19 22:19:44 +0200

    Camera projection support

--- raylib_aisle9/src/camera3d.rs
+++ raylib_aisle9/src/camera3d.rs
@@ -4,15 +4,26 @@ pub struct Camera3D {
     pub camera: raylib_ffi::Camera3D,
 }
 
+pub enum CameraProjection {
+    CameraPerspective = 0,
+    CameraOrthographic = 1,
+}
+
 impl Camera3D {
-    pub fn new(position: Vector3, target: Vector3, up: Vector3, fovy: f32) -> Camera3D {
+    pub fn new(
+        position: Vector3,
+        target: Vector3,
+        up: Vector3,
+        fovy: f32,
+        projection: CameraProjection,
+    ) -> Camera3D {
         Camera3D {
             camera: raylib_ffi::Camera3D {
                 position: position.raw(),
                 target: target.raw(),
                 up: up.raw(),
                 fovy,
-                projection: 0,
+                projection: projection as i32,
             },
         }
     }
