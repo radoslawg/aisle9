87d5099f5c5f50f3a1ee9b820531ef7056e78350
Radoslaw Grzanka <radoslawg@gmail.com>
2026-06-19 22:20:59 +0200

    Very, very early Asset manager loading functionality

--- aisle9/src/asset_manager.rs
+++ aisle9/src/asset_manager.rs
@@ -1,20 +1,46 @@
-use std::{collections::HashMap, sync::OnceLock};
+use std::{collections::HashMap, path::Path, sync::OnceLock};
 
-use raylib_aisle9::texture2d::Texture2D;
+use raylib_aisle9::{load_texture, model::Model, texture2d::Texture2D};
+
+use crate::assets_config::Assets;
 
 pub static ASSET_MANAGER: OnceLock<AssetManager> = OnceLock::new();
 
 pub struct AssetManager {
-    textures: HashMap<String, Texture2D>,
+    pub models: HashMap<String, Model>,
+    pub textures: HashMap<String, Texture2D>,
 }
 
 impl AssetManager {
     pub fn init() {
-        let mut testeroni = HashMap::new();
+        let hjson_text = std::fs::read_to_string("assets/assets.hjson")
+            .expect("failed to read assets.hjson in AssetManager::init");
+        let assets: Assets =
+            deser_hjson::from_str(&hjson_text).expect("failed to parse assets.hjson");
+
+        println!(
+            "Loading textures from assets.hjson... {:?}",
+            assets.textures
+        );
+        let mut textures = HashMap::new();
+        let file_name = &&assets.textures.get("Grid").unwrap().file;
+        let texture_path = Path::new("assets/textures").join(file_name);
+        let texture = load_texture(texture_path.as_path());
+        textures.insert(String::from("grid"), texture);
+
+        let mut models = HashMap::new();
+
+        println!("Loading models from assets.hjson... {:?}", assets.models);
+        let file_name = &assets.models.get("Floor").unwrap().file;
+        let model_path = Path::new("assets/models").join(file_name);
+        let mut model = Model::load_model(model_path.as_path());
+        model
+            .get_material(1)
+            .unwrap()
+            .set_texture(raylib_aisle9::material::MaterialMapIndex::Albedo, texture);
+        models.insert(String::from("floor"), model);
         ASSET_MANAGER
-            .set(AssetManager {
-                textures: testeroni,
-            })
+            .set(AssetManager { models, textures })
             .is_err()
             .then(|| {
                 panic!("AssetManager has already been initialized");
--- aisle9/src/main.rs
+++ aisle9/src/main.rs
@@ -1,34 +1,59 @@
-use anyhow::{Context, Result};
-use raylib_aisle9::colors::*;
+use anyhow::Result;
+use raylib_aisle9::camera3d::CameraProjection;
+use raylib_aisle9::model::WorldModel;
+use raylib_aisle9::vector::Vector3;
 use raylib_aisle9::window::Window;
 use raylib_aisle9::*;
+use raylib_aisle9::{camera3d::Camera3D, colors::*};
 
-use crate::asset_manager::{ASSET_MANAGER, AssetManager};
+use crate::asset_manager::AssetManager;
 
 mod asset_manager;
+mod assets_config;
 
 const WINDOW_WIDTH: u32 = 1280;
 const WINDOW_HEIGHT: u32 = 720;
 const WINDOW_TITLE: &str = "Aisle9";
 
 pub fn main() -> Result<()> {
-    AssetManager::init(); // Initialize the asset manager
+    let window = Window::new(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
     // Try to fix paths when running directly from the workspace root or the debug/release target directory
     if let Ok(exe_path) = std::env::current_exe()
         && let Some(exe_dir) = exe_path.parent()
-        && exe_dir.join("resources").exists()
+        && exe_dir.join("assets").exists()
     {
         let _ = std::env::set_current_dir(exe_dir);
     }
 
-    let window = Window::new(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE);
+    AssetManager::init(); // Initialize the asset manager
+    let floor = AssetManager::get().models.get("floor").unwrap();
+    let camera = Camera3D::new(
+        Vector3::new(50.0, 50.0, 50.0),
+        Vector3::origin(),
+        Vector3::new(0.0, 1.0, 0.0),
+        5.0,
+        CameraProjection::CameraOrthographic,
+    );
+
     while !(window.should_close()) {
         draw(|| {
             // Render text and a background
             clear_background(SKYBLUE);
-            draw_text("Aisle9! Now in Rust's safe mode!", 210, 200, 20, BLACK);
+            mode_3d(&camera, || {
+                for x in -5..6 {
+                    for z in -5..6 {
+                        WorldModel::new(floor)
+                            .with_position(Vector3 {
+                                x: x as f32 * 2.0,
+                                y: 0.0,
+                                z: z as f32 * 2.0,
+                            })
+                            .draw_model();
+                    }
+                }
+            });
+            draw_fps(10, 10);
         });
-        draw_fps(10, 10);
     }
     Ok(())
 }
